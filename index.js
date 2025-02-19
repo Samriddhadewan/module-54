3128cd0960cbd3a27ae67dfdff6c1daa11957918
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-20 01:14:59 +0600

    video 8 done

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const users = [
     {id:2 , name:"sabnoor", email:"sabnoor@gmail.com"},
     {id:3 , name:"sabila", email:"sabila@gmail.com"},
 ]
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 app.get("/", (req, res)=> {
     res.send("User management server is running")
@@ -16,10 +17,14 @@ app.get("/", (req, res)=> {
 
 app.get("/users", (req, res)=>{
     res.send(users);
-})
+}) 
 
 app.post("/users", (req, res)=> {
-    console.log(req.body)
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser);
 })
 
 app.listen(port, ()=>{
