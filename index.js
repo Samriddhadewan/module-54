f0ddda598fa6881e442e50fa05a0e1c8952fe808
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-02-20 00:37:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000;
+
+const users = [
+    {id:1 , name:"sabana", email:"sabana@gmail.com"},
+    {id:2 , name:"sabnoor", email:"sabnoor@gmail.com"},
+    {id:3 , name:"sabila", email:"sabila@gmail.com"},
+]
+app.use(cors())
+
+app.get("/", (req, res)=> {
+    res.send("User management server is running")
+})
+
+app.get("/users", (req, res)=>{
+    res.send(users);
+})
+
+app.post("/users", (req, res)=> {
+    console.log(req.body)
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
